aa4b42df97d92dac696284714dff474bbe93af01
bananaNuggets75 <gazokenan@gmail.com>
2024-05-28 21:36:07 +0800

    membership level dropdown

--- personalTrainerApp/personal_trainer_app/lib/membership_page.dart
+++ personalTrainerApp/personal_trainer_app/lib/membership_page.dart
@@ -1,27 +1,54 @@
 import 'package:flutter/material.dart';
 
-
 class MembershipPage extends StatefulWidget {
   @override
   _MembershipPageState createState() => _MembershipPageState();
 }
 
 class _MembershipPageState extends State<MembershipPage> {
+  String selectedMembership = 'Membership level';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-        title: Text('Memberships'),
-    leading: Builder(
-    builder: (context) {
-    return IconButton(
-    icon: Icon(Icons.menu),
-    onPressed: () {
-    Scaffold.of(context).openDrawer();
-    },
-    );
-    },
-    ),
-    ));
+          title: Text('Memberships'),
+          leading: Builder(
+            builder: (context) {
+              return IconButton(
+                icon: Icon(Icons.menu),
+                onPressed: () {
+                  Scaffold.of(context).openDrawer();
+                },
+              );
+            },
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Select Field:'),
+              DropdownButton<String>(
+                value: selectedMembership,
+                items: <String>['Premium', 'Gold', 'Regular']
+                    .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                onChanged: (String? newValue) {
+                  if (newValue != null) {
+                    setState(() {
+                      selectedMembership = newValue;
+                    });
+                  }
+                },
+              ),
+            ],
+          ),
+        ));
   }
 }
