306cbfdf0dd5745910e652e4233317ac95e185e3
bananaNuggets75 <gazokenan@gmail.com>
2024-05-29 22:51:44 +0800

    user profile picture

--- personalTrainerApp/personal_trainer_app/lib/profile_page.dart
+++ personalTrainerApp/personal_trainer_app/lib/profile_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class ProfilePage extends StatefulWidget {
   @override
   _ProfilePageState createState() => _ProfilePageState();
@@ -10,18 +9,53 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Profile'),
-          leading: Builder(
-            builder: (context) {
-              return IconButton(
-                icon: Icon(Icons.menu),
-                onPressed: () {
-                  Scaffold.of(context).openDrawer();
-                },
-              );
-            },
-          ),
-        ));
+      appBar: AppBar(
+        title: Text('Profile'),
+        leading: Builder(
+          builder: (context) {
+            return IconButton(
+              icon: Icon(Icons.menu),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+            );
+          },
+        ),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: Column(
+                children: [
+                  // User profile image
+                  CircleAvatar(
+                    radius: 50,
+                    backgroundImage: AssetImage('assets/profile_image.png'),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'Joni Jupiter', // User name
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    'Fitness Enthusiast', // User tagline
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.grey[600],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
